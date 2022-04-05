c02b121c1013e1493f14bfe31a57d22c3e9988ca
Ali Qadimi <aliqadimi19988@gmail.com>
2022-04-05 15:57:02 +0430

    modified UserController

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,18 +3,40 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
+use App\Repositories\UserRepositoryInterface;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    public function index(){
-		$data = User::all();
+    //old
+    // public function index(){
+	// 	$data = User::all();
+    //     dd($data);
+	// 	// return view('users.index')->with('data', $data);
+	// }
+
+	// public function show($id){
+	// 	$data = User::findOrFail($id);
+    //     dd($data);
+	// 	// return view('users.show')->with('data', $data);
+	// }
+
+    //new
+    private $repository;
+
+	public function __construct(UserRepositoryInterface $repository)
+	{
+	   $this->repository = $repository;
+	}
+
+	public function index(){
+		$data = $this->repository->getAll();
         dd($data);
 		// return view('users.index')->with('data', $data);
 	}
 
 	public function show($id){
-		$data = User::findOrFail($id);
+		$data = $this->repository->getUser($id);
         dd($data);
 		// return view('users.show')->with('data', $data);
 	}
