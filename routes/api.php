c9f60723aa16a4ea312cba0de84f3f13c47a0eb5
Ali Qadimi <aliqadimi19988@gmail.com>
2022-04-20 15:45:29 +0430

    Create Controller Files

--- routes/api.php
+++ routes/api.php
@@ -19,9 +19,14 @@ use App\Http\Controllers\ArticleController;
 // Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 //     return $request->user();
 // });
-Route::middleware('auth:api')->group(['prefix'=>'v1'], function(){
+// Route::middleware('auth:api')->group(['prefix'=>'v1'], function(){
 
-       Route::resource('products',ProductController::class);
-       Route::resource('articles',ArticleController::class);
+//        Route::resource('articles',ArticleController::class);
 
+// });
+
+Route::post('register', 'RegisterController@register');
+
+Route::middleware('auth:api')->group( function () {
+	Route::resource('products',ProductController::class);
 });
